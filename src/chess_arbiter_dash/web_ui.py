1065f8b18b62bb033c8f3028980d83eab83846fe
Manu(Manu11-Pro) <194583214+Manu11-Pro@users.noreply.github.com>
2026-04-26 12:16:25 +0530

    Refactor: Added Add_game Button and other gui logic

--- src/chess_arbiter_dash/web_ui.py
+++ src/chess_arbiter_dash/web_ui.py
@@ -7,14 +7,3 @@ st.markdown("## Welcome to Chess_Arbiter_Dash!")
 
 add_game = st.sidebar.button("+ Add Game")
 game_add = False
-
-
-# while True:
-#     if add_game :
-#         game_add = True
-#         print(game_add)
-#         time.sleep(10)
-#     else:
-#         game_add = False
-#         print(game_add)
-#         time.sleep(10)
