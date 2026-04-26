95f44d5d57d3991f7810d817d00b6c58e43b4a3c
Manu(Manu11-Pro) <194583214+Manu11-Pro@users.noreply.github.com>
2026-04-26 13:30:30 +0530

    Refactor: added session state and username input

--- src/chess_arbiter_dash/logic.py
+++ src/chess_arbiter_dash/logic.py
@@ -1,1 +1,5 @@
+import berserk
 from utils import game_add
+
+if game_add == True:
+    print()
--- src/chess_arbiter_dash/web_ui.py
+++ src/chess_arbiter_dash/web_ui.py
@@ -7,21 +7,33 @@ st.markdown("## Welcome to Chess_Arbiter_Dash!")
 
 add_game_bt = st.sidebar.button("+ Add Game")
 game_add = False
+typed_in_both = False
+
+if "usernames_container" not in st.session_state:
+    st.session_state.usernames_container = False
 
 if add_game_bt == True:
+    st.session_state.usernames_container = True
     game_add = True
-    with st.container(key="game_view", border=True, autoscroll=True):
-        st.markdown("view_the_games")
 
+if st.session_state.get("usernames_container"):
+    with st.container(key="usernames_input", border=True, autoscroll=True):
+            player_1 = st.text_input(label="Type Username of the player1", value="", placeholder="Username")
+            player_2 = st.text_input(label="Type Username of the player2", value="", placeholder="Username")
+            typed_in_both = st.button("See Game")
+            
+if typed_in_both == True:
+     st.session_state.usernames_container = False
+     
 
 #CSS
 
 st.markdown("""
     <style>
-        .game_view{
+        .usernames_input{
             width: 50%;
             height: 25vh;
             border: 10px solid #FFFFFF;
-        }        
+        }    
     </style>
 """, unsafe_allow_html=True)
