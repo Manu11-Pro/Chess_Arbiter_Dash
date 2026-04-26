075f1947485443236d3be5690b4a25f9ef84bfbf
Manu(Manu11-Pro) <194583214+Manu11-Pro@users.noreply.github.com>
2026-04-26 15:36:07 +0530

    Refactor: Added Insert Token Page

--- src/chess_arbiter_dash/logic.py
+++ src/chess_arbiter_dash/logic.py
@@ -1,5 +1,11 @@
 import berserk
 from utils import player_1, player_2
 
+token = input("Enter your token: ")
+
+def is_username_valid(p1, p2):
+    print
+
+
 print(player_1, player_2)
     
--- src/chess_arbiter_dash/web_ui.py
+++ src/chess_arbiter_dash/web_ui.py
@@ -6,9 +6,11 @@ st.title("♟Chess_Arbiter_Dash♟")
 st.markdown("## Welcome to Chess_Arbiter_Dash!")
 
 add_game_bt = st.sidebar.button("+ Add Game")
+token_bt = st.sidebar.button("Insert Your Token")
 game_add = False
 typed_in_both = False
 
+#Add & View Game
 if "usernames_container" not in st.session_state:
     st.session_state.usernames_container = False
 
@@ -20,13 +22,23 @@ if st.session_state.get("usernames_container"):
     with st.container(key="usernames_input", border=True, autoscroll=True):
             player_1 = st.text_input(label="Type Username of the player1", value="", placeholder="Username")
             player_2 = st.text_input(label="Type Username of the player2", value="", placeholder="Username")
-            st.button("See Game")
-            
-# if _ == True:
-#      st.session_state.usernames_container = False
-#      with st.container(key="game_view", border=True, autoscroll=True):
-#           st.markdown("View the Game!")
-     
+
+# if st.button("Confirm and View Game")== True:
+#      print
+
+#Insert Token
+if "token_container" not in st.session_state:
+    st.session_state.token_container = False
+
+if token_bt == True:
+    st.session_state.token_container = True
+
+if st.session_state.get("token_container"):
+    with st.container(key="token_input", border=True, autoscroll=True):
+            player_1 = st.text_input(label="Insert Your Token", value="", placeholder="Token")
+
+# if st.button("Confirm Token")== True:
+#      print
 
 #CSS
 
@@ -43,5 +55,11 @@ st.markdown("""
             height: 25vh;
             border: 10px solid #FFFFFF;
         }  
+            
+        .token_container{
+            width: 50%;
+            height: 25vh;
+            border: 10px solid #FFFFFF;
+        }
     </style>
 """, unsafe_allow_html=True)
